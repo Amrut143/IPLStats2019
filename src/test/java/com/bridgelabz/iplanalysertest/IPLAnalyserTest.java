258ef587d93cc3681e8de7f60badf7edff1c1ab6
Amrut <amrutpanda7789@gmail.com>
2020-07-08 09:05:37 +0530

    ADDED: Given wrong csv file path for most run should return costum exception

--- src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
@@ -1,16 +1,35 @@
 package com.bridgelabz.iplanalysertest;
 
+import com.bridgelabz.iplanalyser.exception.IPLAnalyserException;
 import com.bridgelabz.iplanalyser.service.IPLAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IPLAnalyserTest {
     private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String WROMG_MOST_RUNS_CSV_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostRuns.csv";
+
 
     @Test
     public void givenIPLMostRunsCSVFile_ShouldReturnCorrectRecords() {
-        IPLAnalyser iplAnalyser = new IPLAnalyser();
-        int numOfRecords = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
-        Assert.assertEquals(100,numOfRecords);
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            int numOfRecords = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+            Assert.assertEquals(100, numOfRecords);
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenWrongIPLMOstRunsCSVFile_ShouldReturnCustomExceptionType() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(WROMG_MOST_RUNS_CSV_FILE_PATH);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM, e.type);
+            e.printStackTrace();
+        }
+
     }
 }
