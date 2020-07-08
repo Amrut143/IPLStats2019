96adca7de8884a48f60115aa5fc9bc8ebc040f1b
Amrut <amrutpanda7789@gmail.com>
2020-07-08 22:50:40 +0530

    ADDED: Given most wickets csv file with incorrect delimiter and header should return custom exception

--- src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
+++ src/test/java/com/bridgelabz/iplanalysertest/IPLAnalyserTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 
 public class IPLAnalyserTest {
     private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
-    private static final String WROMG_MOST_RUNS_CSV_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String WRONG_MOST_RUNS_CSV_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_MOST_WKTS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
+    private static final String WRONG_MOST_WKTS_CSV_FILE_PATH = "./src/main/resources/IPL2019FactsheetMostWkts.csv";
+
 
 
     @Test
@@ -25,7 +28,7 @@ public class IPLAnalyserTest {
     public void givenWrongIPLMostRunsCSVFile_ShouldReturnCustomExceptionType() {
         try {
             IPLAnalyser iplAnalyser = new IPLAnalyser();
-            iplAnalyser.loadIPLMostRunsData(WROMG_MOST_RUNS_CSV_FILE_PATH);
+            iplAnalyser.loadIPLMostRunsData(WRONG_MOST_RUNS_CSV_FILE_PATH);
         } catch (IPLAnalyserException e) {
             Assert.assertEquals(IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM, e.type);
             e.printStackTrace();
@@ -48,6 +51,50 @@ public class IPLAnalyserTest {
         try {
             IPLAnalyser iplAnalyser = new IPLAnalyser();
             iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUE, e.type);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIPLMostWktsCSVFile_ShouldReturnCorrectRecords() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            int numOfRecords = iplAnalyser.loadIPLMostWktsData(IPL_MOST_WKTS_CSV_FILE_PATH);
+            Assert.assertEquals(99, numOfRecords);
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenWrongIPLMostWktsCSVFile_ShouldReturnCustomExceptionType() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(WRONG_MOST_WKTS_CSV_FILE_PATH);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.IPL_FILE_PROBLEM, e.type);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIPLMostWktsCSVFile_WhenCorrect_ButDelimiterIncorrect_ShouldThrowException() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(IPL_MOST_WKTS_CSV_FILE_PATH);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUE, e.type);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIPLMostWktsCSVFile_WhenCorrect_ButHeaderIncorrect_ShouldThrowException() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(IPL_MOST_WKTS_CSV_FILE_PATH);
         } catch (IPLAnalyserException e) {
             Assert.assertEquals(IPLAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUE, e.type);
             e.printStackTrace();
