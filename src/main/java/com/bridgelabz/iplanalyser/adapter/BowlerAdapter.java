d8a30840a415154908152ebfa59b143b292a8dd9
Amrut <amrutpanda7789@gmail.com>
2020-07-09 21:12:44 +0530

    REFACTORED: Code to load bowler data in bowler adapter

--- src/main/java/com/bridgelabz/iplanalyser/adapter/BowlerAdapter.java
+++ src/main/java/com/bridgelabz/iplanalyser/adapter/BowlerAdapter.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.iplanalyser.adapter;
 
-public class BowlerAdapter {
+import com.bridgelabz.iplanalyser.dao.IPLRecordDAO;
+import com.bridgelabz.iplanalyser.exception.IPLAnalyserException;
+import com.bridgelabz.iplanalyser.model.IPLBowlerDataCSV;
+import com.bridgelabz.iplanalyser.utility.PlayerType;
+
+import java.util.Map;
+
+public class BowlerAdapter extends IPLAdapter {
+
+    /**
+     *
+     * @param playerType
+     * @param csvFilePath
+     * @return
+     * @throws IPLAnalyserException
+     */
+    @Override
+    public Map<String, IPLRecordDAO> loadIPLData(PlayerType playerType, String... csvFilePath) throws IPLAnalyserException {
+        Map<String, IPLRecordDAO> recordDAOMap = super.loadIPLData(IPLBowlerDataCSV.class, csvFilePath);
+        return recordDAOMap;
+    }
 }
